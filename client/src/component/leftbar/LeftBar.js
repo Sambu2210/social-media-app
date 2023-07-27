2bfcf088d111ddc940ef9c3818c45f1c92f62083
Sambu2210 <sanjai007007007@gmail.com>
2023-07-27 11:23:44 +0530

    created left bar adding some menu images

--- client/src/component/leftbar/LeftBar.js
+++ client/src/component/leftbar/LeftBar.js
@@ -1,8 +1,108 @@
 import "./LeftBar.css";
+import friends from "../../assets/1.png";
+import group from "../../assets/2.png";
+import marketplace from "../../assets/3.png";
+import watch from "../../assets/4.png";
+import memories from "../../assets/5.png";
+import event from "../../assets/6.png";
+import gaming from "../../assets/7.png";
+import gallary from "../../assets/8.png";
+import videos from "../../assets/9.png";
+import messages from "../../assets/10.png";
+import tutorials from "../../assets/11.png";
+import courses from "../../assets/12.png";
+import fund from "../../assets/13.png";
 const LeftBar = () => {
   return (
-    <div>
-      <h1>LeftBar</h1>
+    <div className="leftbar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt="person"
+              style={{ width: "30px", height: "30px" }}
+            />
+            <span>Balaji</span>
+          </div>
+          <div className="items">
+            <img src={friends} alt="" />
+            <span>Friends</span>
+          </div>
+          <div className="items">
+            <img src={group} alt="" />
+            <span>Group</span>
+          </div>
+          <div className="items">
+            <img src={marketplace} alt="" />
+            <span>Marketplace</span>
+          </div>
+          <div className="items">
+            <img src={watch} alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="items">
+            <img src={memories} alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Shortcuts</span>
+          <div className="items">
+            <div className="items">
+              <img src={event} alt="" />
+              <span>Event</span>
+            </div>
+          </div>
+          <div className="items">
+            <div className="items">
+              <img src={gaming} alt="" />
+              <span>Gaming</span>
+            </div>
+          </div>
+          <div className="items">
+            <div className="items">
+              <img src={gallary} alt="" />
+              <span>Gallary</span>
+            </div>
+          </div>
+          <div className="items">
+            <div className="items">
+              <img src={videos} alt="" />
+              <span>Videos</span>
+            </div>
+          </div>
+          <div className="items">
+            <div className="items">
+              <img src={messages} alt="" />
+              <span>Messages</span>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          <div className="items">
+            <div className="items">
+              <img src={tutorials} alt="" />
+              <span>Tutorials</span>
+            </div>
+          </div>
+          <div className="items">
+            <div className="items">
+              <img src={courses} alt="" />
+              <span>Courses</span>
+            </div>
+          </div>
+          <div className="items">
+            <div className="items">
+              <img src={fund} alt="" />
+              <span>Fund</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
