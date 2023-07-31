49589696f4eae87211acde8c895b9b30578df730
Sambu2210 <sanjai007007007@gmail.com>
2023-07-31 12:53:34 +0530

    updating the profile page content

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,7 +1,57 @@
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import PlaceIcon from "@mui/icons-material/Place";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import LanguageIcon from "@mui/icons-material/Language";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import "./Profile.css";
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.unsplash.com/photo-1581832097375-e95cc1fcb3d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          alt="profile image"
+          className="cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1575516092328-522b5e8d3ec7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+          alt="profile image"
+          className="profilepic"
+        />
+      </div>
+      <div className="porfile-container">
+        <div className="uinfo">
+          <div className="left">
+            <FacebookIcon fontSize="large" />
+            <InstagramIcon fontSize="large" />
+            <LinkedInIcon fontSize="large" />
+            <TwitterIcon fontSize="large" />
+            <PinterestIcon fontSize="large" />
+          </div>
+          <div className="center">
+            <span>Balaji</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>lamp.dev</span>
+              </div>
+              <button>follow</button>
+            </div>
+          </div>
+          <div className="right">
+            <MailOutlineIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
